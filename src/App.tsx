eb11ccf51e8ae7c4083a26c590cec6e047d2cded
Donald Ivan Cribillo <developer.donaldivan@gmail.com>
2023-08-03 23:37:06 +0800

    Tast #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import NeoGrid from './Grid';
+import NeoGrid from "./components/Grid";
+import Header from "./components/Header";
 
 const App = () => {
   return (
+    <>
+      <Header />
       <NeoGrid />
+    </>
   );
-}
+};
 
 export default App;
