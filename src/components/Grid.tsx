74ad16efdaa74f7ae109043772c259add8c8cbcc
Donald Ivan Cribillo <developer.donaldivan@gmail.com>
2023-08-04 00:29:39 +0800

    Tast #2

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -3,24 +3,94 @@ import { ColDef } from "ag-grid-community";
 import data from "../near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+const stringFilter = {
+  filterOptions: ["contains", "notContains"],
+  textFormatter: (r: string) => {
+    if (!r) return null;
+    return r.toLowerCase();
+  },
+  debounceMs: 200,
+  maxNumConditions: 1,
+};
+
+const numberFilter = {
+  filter: "agNumberColumnFilter",
+  filterParams: {
+    allowedCharPattern: "\\d\\-\\,",
+    numberParser: (text: string) => {
+      return text ? parseFloat(text.replace(",", ".").replace("$", "")) : null;
+    },
+  },
+};
+
+const dateFilter = {
+  filter: "agDateColumnFilter",
+  filterParams: {
+    comparator: (filterLocalDateAtMidnight: Date, cellValue: string) => {
+      if (!cellValue) return -1;
+      const cellDate = new Date(cellValue);
+      if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
+        return 0;
+      }
+      if (cellDate < filterLocalDateAtMidnight) {
+        return -1;
+      }
+      if (cellDate > filterLocalDateAtMidnight) {
+        return 1;
+      }
+      return 0;
+    },
+  },
+};
 
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
+  {
+    field: "designation",
+    headerName: "Designation",
+    filterParams: stringFilter,
+  },
+  { field: "discovery_date", headerName: "Discovery Date", ...dateFilter },
+  {
+    field: "h_mag",
+    headerName: "H (mag)",
+    ...numberFilter,
+  },
+  { field: "moid_au", headerName: "MOID (au)", ...numberFilter },
+  { field: "q_au_1", headerName: "q (au)", ...numberFilter },
+  { field: "q_au_2", headerName: "Q (au)", ...numberFilter },
+  { field: "period_yr", headerName: "Period (yr)", ...numberFilter },
+  {
+    field: "i_deg",
+    headerName: "Inclination (deg)",
+    ...numberFilter,
+  },
+  {
+    field: "pha",
+    headerName: "Potentially Hazardous",
+    filterParams: stringFilter,
+  },
+  {
+    field: "orbit_class",
+    headerName: "Orbit Class",
+    enableRowGroup: true,
+    filterParams: stringFilter,
+  },
 ];
+
+const defaultColDef: ColDef = {
+  sortable: true,
+  filter: true,
+  flex: 1,
+};
 
 const NeoGrid = (): JSX.Element => {
   return (
-    <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+    <div
+      className="ag-theme-alpine"
+      style={{ height: 900, width: "100%", minWidth: 1920 }}
+    >
       <AgGridReact
+        defaultColDef={defaultColDef}
         rowData={data}
         columnDefs={columnDefs}
         rowGroupPanelShow={"always"}
